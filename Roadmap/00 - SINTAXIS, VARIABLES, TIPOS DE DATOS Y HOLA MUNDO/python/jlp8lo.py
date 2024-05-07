4ab1494878c38c0683d548f287f1e873bef8324b
jlp8lo <74940158+jlp8lo@users.noreply.github.com>
2024-05-06 23:23:40 -0400

    jlp8lo.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jlp8lo.py
@@ -0,0 +1,28 @@
+# https://python.org
+
+# Para comentarios en una línea
+
+"""
+Para comentarios
+en varias
+líneas
+"""
+
+'''
+Varias
+Lineas
+'''
+
+my_variable = "Presentando una variable"
+my_variable = "Nuevo valor de variable"
+
+MY_CONSTANT = "Mi constante" # por convención
+
+my_int = 36
+my_float = 9.5
+my_bool = True
+my_bool = False
+my_string = "con comillas dobles"
+my_other_string = 'con comillas simples'
+
+print(¡Hola, Python!)
